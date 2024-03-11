a2b6e136e420233225a9c5f998c8faed46a66a79
felipe-lad <felipe.ladino@letsdoitnow.us>
2024-03-11 14:20:26 -0500

    fix(companies): remove unnecessary endpoints

--- dist/modules/companies/companies.controller.d.ts
+++ dist/modules/companies/companies.controller.d.ts
@@ -1,33 +1,5 @@
-/// <reference types="mongoose/types/aggregate" />
-/// <reference types="mongoose/types/callback" />
-/// <reference types="mongoose/types/collection" />
-/// <reference types="mongoose/types/connection" />
-/// <reference types="mongoose/types/cursor" />
-/// <reference types="mongoose/types/document" />
-/// <reference types="mongoose/types/error" />
-/// <reference types="mongoose/types/expressions" />
-/// <reference types="mongoose/types/helpers" />
-/// <reference types="mongoose/types/middlewares" />
-/// <reference types="mongoose/types/indexes" />
-/// <reference types="mongoose/types/models" />
-/// <reference types="mongoose/types/mongooseoptions" />
-/// <reference types="mongoose/types/pipelinestage" />
-/// <reference types="mongoose/types/populate" />
-/// <reference types="mongoose/types/query" />
-/// <reference types="mongoose/types/schemaoptions" />
-/// <reference types="mongoose/types/schematypes" />
-/// <reference types="mongoose/types/session" />
-/// <reference types="mongoose/types/types" />
-/// <reference types="mongoose/types/utility" />
-/// <reference types="mongoose/types/validation" />
-/// <reference types="mongoose/types/virtuals" />
-/// <reference types="mongoose/types/inferschematype" />
-import { Types } from 'mongoose';
 import { CompaniesService } from './companies.service';
 export declare class CompaniesController {
     private readonly companiesService;
     constructor(companiesService: CompaniesService);
-    findAllByWorkerId(workerId: Types.ObjectId): Promise<{
-        data: any[];
-    }>;
 }
--- dist/modules/companies/companies.controller.js
+++ dist/modules/companies/companies.controller.js
@@ -8,31 +8,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CompaniesController = void 0;
 const common_1 = require("@nestjs/common");
-const mongoose_1 = require("mongoose");
-const mongo_id_pipe_1 = require("../../pipes/mongo-id.pipe");
 const companies_service_1 = require("./companies.service");
 let CompaniesController = class CompaniesController {
     constructor(companiesService) {
         this.companiesService = companiesService;
     }
-    async findAllByWorkerId(workerId) {
-        return this.companiesService.findAllByWorkerId(workerId);
-    }
 };
 exports.CompaniesController = CompaniesController;
-__decorate([
-    (0, common_1.Get)('by-worker/:id'),
-    __param(0, (0, common_1.Param)('id', mongo_id_pipe_1.ParseMongoIdPipe)),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [mongoose_1.Types.ObjectId]),
-    __metadata("design:returntype", Promise)
-], CompaniesController.prototype, "findAllByWorkerId", null);
 exports.CompaniesController = CompaniesController = __decorate([
     (0, common_1.Controller)('companies'),
     __metadata("design:paramtypes", [companies_service_1.CompaniesService])
